409261d0722a7b6d52030199796545bac4be193d
unLomTrois <vadim.kuz02@gmail.com>
2020-04-10 22:22:57 +0300

    fix add license get for random

--- get/random.sh
+++ get/random.sh
@@ -1,3 +1,3 @@
 mkdir ../include/effolkronium
 curl -o ../include/effolkronium/random.hpp https://raw.githubusercontent.com/effolkronium/random/master/include/effolkronium/random.hpp
-
+curl -o ../include/effolkronium/LICENSE.MIT https://raw.githubusercontent.com/effolkronium/random/master/LICENSE.MIT
